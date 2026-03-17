4463715c37069ab207aec2a949f5d84c804377f4
tkatchuk.eugenio <eugenio.tkatchuk@gmail.com>
2026-03-17 16:39:15 -0300

    Código perfeito, limpo e sem o basePath do GitHub

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,4 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  output: 'export', // Necessário para GitHub Pages
-  basePath: '/site-daiane-camargo',
-  assetPrefix: '/site-daiane-camargo',
-  images: {
-    unoptimized: true, // Necessário para imagens estáticas no GitHub
-  },
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
 export default nextConfig;
